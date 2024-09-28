de2d3cfb5590006aabba6114e91e25b9a1c27ecc
blenhenok <79223969+blenhenok@users.noreply.github.com>
2024-09-28 20:20:00 +0300

    starting

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className="logo">
+            <h1><span>B</span>len</h1>
+        </div>
+        <nav>
+            <button></button>
+            <ul>
+                <li><a href='/' >Home</a></li>
+                <li><a href='/' >About</a></li>
+                <li><a href='/' >Experience</a></li>
+                <li><a href='/' >Work</a></li>
+                <li><a href='/' >Contact</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+    </>
+    
+  )
+}
+export default Nav;
